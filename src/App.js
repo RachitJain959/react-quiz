529b2e7b9828f22cfe53af54f90cb61668c5674a
Rachit Jain <rachitjain9596@gmail.com>
2024-02-13 22:23:57 +0530

    add data & useReducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
 
+const initialState = {
+  questions: [],
+
+  // 'loading', 'ready', 'active', 'finished', 'error'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+
+    default:
+      throw new Error('Action unknown');
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function fetchData() {
+      const res = await fetch('http://localhost:8000/questions');
+      const data = await res.json();
+      dispatch({ type: 'dataReceived', payload: data });
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="app">
       <Header />
