cb430638d97ee391f6a1a028b2f13a207b3557c8
Rachit Jain <rachitjain9596@gmail.com>
2024-02-14 00:34:56 +0530

    Display questions p3-options bugfix

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,9 +1,11 @@
-function Options() {
+function Options({ question }) {
   return (
     <div>
       <div className="options">
         {question.options.map((option) => (
-          <button className="btn btn-option">{option}</button>
+          <button className="btn btn-option" key={option}>
+            {option}
+          </button>
         ))}
       </div>
     </div>
