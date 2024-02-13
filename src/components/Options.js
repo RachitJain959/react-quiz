051ecace37ae46d27a0096b0139b6dda92f111cf
Rachit Jain <rachitjain9596@gmail.com>
2024-02-14 01:09:38 +0530

    Handle Answers p2-correct incorrect option

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,7 +4,15 @@ function Options({ question, dispatch, answer }) {
       <div className="options">
         {question.options.map((option, index) => (
           <button
-            className="btn btn-option"
+            className={`btn btn-option ${
+              answer === index &&
+              answer !== question.correctOption &&
+              'answer wrong'
+            } ${
+              answer === index &&
+              answer === question.correctOption &&
+              'answer correct'
+            } ${index !== question.correctOption ? 'other' : 'correct'}`}
             key={option}
             onClick={() => dispatch({ type: 'newAnswer', payload: index })}
           >
