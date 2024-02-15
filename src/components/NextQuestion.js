79996ffa6a4c730b1b0207a4f45a926062d26d3d
Rachit Jain <rachitjain9596@gmail.com>
2024-02-15 19:45:19 +0530

    Finish component p3-useReducer status

--- src/components/NextQuestion.js
+++ src/components/NextQuestion.js
@@ -1,15 +1,29 @@
-function NextQuestion({ dispatch, answer }) {
+function NextQuestion({ dispatch, answer, index, numQuestions }) {
   if (answer === null) return null;
-  return (
-    <div>
-      <button
-        className="btn btn-ui"
-        onClick={() => dispatch({ type: 'nextQuestion' })}
-      >
-        Next
-      </button>
-    </div>
-  );
+
+  if (index < numQuestions - 1)
+    return (
+      <div>
+        <button
+          className="btn btn-ui"
+          onClick={() => dispatch({ type: 'nextQuestion' })}
+        >
+          Next
+        </button>
+      </div>
+    );
+
+  if (index === numQuestions - 1)
+    return (
+      <div>
+        <button
+          className="btn btn-ui"
+          onClick={() => dispatch({ type: 'finish' })}
+        >
+          Finish
+        </button>
+      </div>
+    );
 }
 
 export default NextQuestion;
